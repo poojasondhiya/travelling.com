61745d1d8a4da339055ab7a9b0d5a8c5bf870088
poojasondhiya <sondhiyapooja41@gmail.com>
2024-12-24 18:01:34 +0530

    finally deployed by rudra

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import India from './pages/India';
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/travelling.com/'
+  base:'/travelling.com/',
+  build:{
+    outDir:'dist',
+    emptyOutDir:true,
+  },
 })
